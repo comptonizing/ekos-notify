002da028f92a8d2833f0236ce5a9c10f681a8a51
Philipp Weber <philipp.ph.weber@fau.de>
2026-03-21 23:00:37 +0100

    Add align new log notifications

--- src/frame.h
+++ src/frame.h
@@ -121,7 +121,8 @@ namespace EN {
                         };
 
                         std::unordered_map<std::string, std::string> m_logInterfaceNotificationMap = {
-                            {"org.kde.kstars.Ekos", "ekosNewLog"}
+                            {"org.kde.kstars.Ekos", "ekosNewLog"},
+                            {"org.kde.kstars.Ekos.Align", "alignNewLog"}
                         };
                         
                         struct SignalData {
